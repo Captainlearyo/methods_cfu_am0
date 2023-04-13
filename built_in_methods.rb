eedee9ab045cfeebe878009354f79dc9982d5c63
Captainlearyo <clayoleary@gmail.com>
2023-04-13 12:51:26 -0700

    Complete

--- built_in_methods.rb
+++ built_in_methods.rb
@@ -1,28 +1,30 @@
 # SECTION 1: Calling methods on string or integer objects.
 # Run each line of code below (either from this file or in IRB individually).
-# Then, in a ruby comment, write 1-2 sentences describing what is happening, using ALL the involved vocabulary terms you've learned in this lesson so far.
+# Then, in a ruby comment, write 1-2 sentences describing what is happening, 
+# using ALL the involved vocabulary terms you've learned in this lesson so far.
 
 # EXAMPLE
 # The downcase method is called on the string object "Hello World"
-# No arguments are passed; downcase has one clear job which is to lowercase all letters that exist in the String
+# No arguments are passed; downcase has one clear job which is to 
+# lowercase all letters that exist in the String
 # The return value is "hello world"
-"Hello World".downcase
+p "Hello World".downcase
+# downcase retuns a string with all letters in lowercase 
 
+p "Hello World".include?("Hello")
+# includes returns a boolean for wether the argument is in the string
 
-"Hello World".include?("Hello")
+p "Hello World".end_with?("Hello")
+# end with returnss a boolean on wether the string ends with the argument or not 
 
+p "Hello World".end_with?("rld")
+# end with returnss a boolean on wether the string ends with the argument or not
 
-"Hello World".end_with?("Hello")
+p 12.even?
+# even returns a boolean on if the intiger is even or not
 
-
-"Hello World".end_with?("rld")
-
-
-12.even?
-
-
-18.next
-
+p 18.next
+# next returns +1 the intger value
 
 
 # SECTION 2: Calling methods on variables assigned to strings.
@@ -46,7 +48,11 @@ puts first_name.start_with?("J")
 # Call a different built-in Ruby method on each of your variables. 
 # https://ruby-doc.org/core-3.1.0/Integer.html
 # Include comments above each method call explaining the impact and return value of that method.
+twelve = 12
+puts twelve.next
 
+ten = 10
+puts ten.even?
 
 
 # SECTION 4: Calling methods on variables assigned to arrays.
@@ -54,3 +60,30 @@ puts first_name.start_with?("J")
 # Call a different built-in Ruby method on each of your variables. 
 # https://ruby-doc.org/core-3.1.0/Array.html
 # Include comments above each method call explaining the impact and return value of that method.
+
+arr1 = [1,2,3,4]
+arr2 = ["one","two","three","four"]
+p arr1
+p arr2
+
+arr3 = []
+
+# sets string values of each element of the array arr2 to the intiger values of arr1
+arr2[0] = arr1[0]
+arr2[1] = arr1[1]
+arr2[2] = arr1[2]
+arr2[3] = arr1[3]
+
+puts arr2.sum # should sum arr1 to 10
+
+p arr2[3].next #should print 5
+
+sum_of_arr1 = arr2.sum
+
+p sum_of_arr1.next.to_s #should print 11 as a sting with partheseis 
+
+arr2.each do |value|
+arr3.push(value)
+end
+
+p arr3.reverse #returns a copy of arr1 reversed 
--- final_practice.rb
+++ final_practice.rb
@@ -7,42 +7,93 @@
 # 1: Write a method named greeting that returns a string with a general greeting. 
 
 # What is the return value of your method?
+#return "hello"
 # How many arguments did you pass your method?
+# 1
 
+
+
+def greeting
+
+    return "hello"
+end
+
+puts greeting
 
 # 2: Write a method named custom_greeting that returns a greeting WITH a specific name.
 
 # What is the return value of your method?
+# return "hello #{name}"
 # How many arguments did you pass your method?
+# 1
 # What data type was your argument(s)?
+# string
 
+def custom_greeting(name)
+
+    return "hello #{name}"
+end
+
+puts custom_greeting("greg")
 
 # 3: Write a method named greet_person that takes in 3 strings, a first, middle, and last name, and returns a sentence with the full name.
 
 # What is the return value of your method?
+#return "hello #{f} #{m} #{l}"
 # How many arguments did you pass your method?
+# 3
 # What data type was your argument(s)?
+# strings
 
+
+def greet_person(f,m,l)
+
+    return "hello #{f} #{m} #{l}"
+end
+
+puts greet_person("greg", "tommy", "machoy")
 
 # 4: Write a method named square that takes in one integer, and returns the square of that integer.
 # Bonus: Print a sentence that interpolates the return value of your square method.
 
 # What is the return value of your method?
+# i^2
 # How many arguments did you pass your method?
+# 1
 # What data type was your argument(s)?
+# intiger
 
+def square(i)
+return i * i
+end
+
+puts square(3)
 
 # 5: Write a method named check_stock that satisfies the following interaction pattern:
 # Hint: You will only write one check_stock method that checks the quantity and then prints the corresponding statement.
 
-check_stock(4, "Coffee");
+def check_stock(num, str)
+    if num == 4 && str == "Coffee"
+        return "Coffee is stocked"
+    elsif num == 3 && str == "Tortillas"
+        return "Tortillas - running LOW"
+    elsif num == 0 && str == "Cheese"
+        return "Cheese - OUT of stock!"
+    elsif num == 1 && str == "Salsa"
+        return "Salsa - running LOW"
+    
+    end
+    end
+
+puts check_stock(4, "Coffee");
 # => "Coffee is stocked"
 
-check_stock(3, "Tortillas");
+puts check_stock(3, "Tortillas");
 # => "Tortillas - running LOW"
 
-check_stock(0, "Cheese");
+puts check_stock(0, "Cheese");
 # => "Cheese - OUT of stock!"
 
-check_stock(1, "Salsa");
+puts check_stock(1, "Salsa");
 # => "Salsa - running LOW"
+
